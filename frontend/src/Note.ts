9230a2d381cacb6b5256586830e8cb3984c33267
Hien Le <hle@skillz.com>
2022-05-02 20:01:35 -0700

    replace type definition yup

--- frontend/src/Note.ts
+++ frontend/src/Note.ts
@@ -1,10 +1,15 @@
+import {object, string, number, date, InferType, ObjectSchema} from 'yup';
+
 export enum NoteCategory {
     request = 'request',
     alert = 'alert',
     comment = 'comment'
 }
 
-export interface Note {
-    note: string;
-    category: NoteCategory;
-}
+export const noteSchema = object({
+    user_id: number().required(),
+    category: string().oneOf(Object.values(NoteCategory)).required(),
+    note: string(),
+})
+
+export type Note = InferType<typeof noteSchema>;

c66d61641f2c8c64ca06ef5279fe8ecd51a3c179
Hien Le <hle@skillz.com>
2022-05-02 19:00:15 -0700

    connect response to grid

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,40 @@
 import React from 'react';
 import './App.css';
-import {Note, NoteCategory} from "./Note";
+import {Note} from "./Note";
 import {useNotes} from "./Notes";
+import {CircularProgress} from "@mui/material";
+import {DataGrid, GridColDef} from "@mui/x-data-grid";
+
+const noteColumns : GridColDef[] = [
+    {field: 'id', headerName: 'ID'},
+    {field: 'user_id', headerName: 'User'},
+    {field: 'category', headerName: 'Category'},
+    {field: 'note', headerName: 'Note', width: 400},
+]
 
 function App() {
+  /*
+  Don't destructure the useQuery() results:
+  1. If there's more than one query, the grouping is more clear.
+  2. Discriminated types! 'isSuccess == true' lets you access 'data' without null-check.
+   */
   const notes = useNotes();
+
   return (
     <div className="App">
         {notes.isLoading && <Loading/>}
-        {notes.isSuccess && <Data notes={notes.data.data}/>}
+        {notes.isSuccess && <DataGrid
+            columns={noteColumns}
+            rows={notes.data.data} rowCount={notes.data.total}
+            pageSize={10}
+        />}
     </div>
   );
 }
 
 function Loading() {
     return (
-        <p>Loading...</p>
-    )
-}
-
-interface DataProps {
-    notes: Note[];
-}
-function Data({notes} : DataProps) {
-    return (
-        <pre>{JSON.stringify(notes)}</pre>
+        <CircularProgress/>
     )
 }
 
